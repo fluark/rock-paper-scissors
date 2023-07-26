f974cba6efbeece2ae739746e833063d0a3bd1cf
Ben <hmcdaniel07@gmail.com>
2023-07-26 08:18:40 -0500

    Add comparisons to play a full round with all move possibilities

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,104 +1,51 @@
-/* following code defines a function */
-
-/*
-function getComputerChoice() {
-    const moves = ["rock", "paper", "scissors"]
-    return moves[Math.floor(Math.random() * moves.length)]
-}
-
-function singleRound (playerSelection, computerSelection) {
-     playerSelection = prompt("Enter move")
-     computerSelection = getComputerChoice()
-
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        alert("You lose! Paper beats rock")
-    }    
-
-    
-}
-
-console.log(getComputerChoice)
-console.log(singleRound) 
-*/
-
-/* function getComputerChoice() { // randomly return R, P or S for comps play
-    const array = [ "rock", "paper", "scissors"]
-    const randomMove = array[(Math.floor(Math.random() * (array.length)))];
-    console.log(randomMove);// checking expected output
-}
-
-function playRound(playerSelection, computerSelection) { 
-    playerSelection = prompt("Enter move")
-
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        alert("You lose! Paper beats rock")
-    }     
-
-    
-    computerSelection = getComputerChoice(); 
-    console.log(playRound(prompt, computerSelection));
-}
-first function works fine, second keeps looping the prompt box, stuck on line 27*/
-
-
 function getComputerChoice() { // randomly return R, P or S for comps play
-    const array = [ "rock", "paper", "scissors"]
+    const array = ["rock", "paper", "scissors"]
     return array[(Math.floor(Math.random() * (array.length)))];
 }
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = prompt("Enter move").toLowerCase();
-    console.log(computerSelection); 
+    console.log(computerSelection);
 
-    if (playerSelection === "rock" && computerSelection === "paper") {
-     return("You lose! Paper beats rock")
-    }
-        else if (computerSelection === "scissors") {
-            return("You win! Rock beats scissors")
+    if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return ("You lose! Scissors beats paper")
         }
-            else if (computerSelection === "rock") {
-                return("It is a tie!")
-            }
+        if (computerSelection === "rock")
+            return ("You win! Paper beats " + computerSelection)
 
-    
-    }    
+        else (playerSelection === computerSelection);
+        return ("It\'s a tie!")
+    }
 
-const playerSelection = "rock";
-let computerSelection = getComputerChoice(); 
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return ("You lose! Paper beats rock")
+        }
+        if (computerSelection === "scissors")
+            return ("You win! Rock beats " + computerSelection)
+
+        else (playerSelection === computerSelection);
+        return ("It\'s a tie!")
+    }
+
+    if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return ("You lose! Rock beats scissors")
+        }
+        if (computerSelection === "paper")
+            return ("You win! Scissors beats " + computerSelection)
+
+        else (playerSelection === computerSelection);
+        return ("It\'s a tie!")
+    }
+
+}
+let playerSelection = "paper"; //string has no effect on move
+let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  
-array in getComputerChoice is a parameter(placeholder), it contains nothing.
-// parameter defines the function
-// the argument is the info that is fed to the funciton
-
-
-You aren't using the argument (value) 
-of computerSelection you're passing to singleRound.
-The function is also not using the parameter computerSelection. */
-
-// I am expecting
-
-
-
-
-/*
-Need to call function getComputerChoice from computerSelection somehow..? 
-Or find a way to relate them? 
-
-Currently sends same alert regardless of player or computer selection 
-and 'enter' exits the window */
+/* copy this code onto desktop. remove repos folder from icloud
+clone remote   RPS repo into repo folder on laptop
+pull changes from github
+push new local changes. */
